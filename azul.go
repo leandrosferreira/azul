6858af94fea762c9926536c9c925316fc14acced
Leandro Ferreira <lferreira@totvs.com.br>
2017-08-11 17:34:31 -0300

    Add basic directory/file structure

---
+++ azul.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import _ "github.com/lsferreira42/azul/api"
+import _ "github.com/lsferreira42/azul/backends"
+import _ "github.com/lsferreira42/azul/caching"
+import _ "github.com/lsferreira42/azul/dns"
+import _ "github.com/lsferreira42/azul/resolver"
+
+func main() {
+	fmt.Println("Hello from Azul")
+}
